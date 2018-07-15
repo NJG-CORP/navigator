d24dc9c502d2f29bc57367fb389ea314b952e34d
UD-DEV <ijoy14@yandex.ru>
2018-07-15 16:25:58 +0300

    Initial commit

---
+++ controllers/TracingController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dev
+ * Date: 15.07.18
+ * Time: 16:21
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class TracingController extends Controller
+{
+    public function actionStore()
+    {
+        return '';
+    }
+}
---
+++ web/js/tracking.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    let timeOpened = Date.now();
+    const urlOpened = window.location.pathname;
+
+    //Слежение за закрытием вкладки
+    window.onbeforeunload = function () {
+        tracker(urlOpened, timeOpened);
+    };
+
+    //Окно потеряло фокус
+    window.onblur = function () {
+        tracker(urlOpened, timeOpened);
+    };
+
+    //Пользователь вернулся на страницу
+    window.onfocus = function () {
+        timeOpened = Date.now();
+    };
+
+    //Слежение за нажатием на ссылку
+    $("a").on("click",function () {
+        //TODO
+        alert('Переход по ссылке');
+    });
+});
+
+function tracker(page, begin) {
+    const time = Date.now() - begin;
+    $.ajax({
+        url: '/tracing',
+        method: 'POST',
+        data: {
+            url: page,
+            time: time
+        },
+        success: function () {
+            //TODO
+        },
+        error: function () {
+            //TODO
+        }
+    });
+}
