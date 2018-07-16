e731a17ddb461911ab3d8b7a4a1c8f2dc62540cb
UD-DEV <ijoy14@yandex.ru>
2018-07-16 16:37:50 +0300

    remove db.php from git

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=project_navigator',
-    'username' => 'root',
-    'password' => '31214461Aa',
-    'charset' => 'utf8',
-];
